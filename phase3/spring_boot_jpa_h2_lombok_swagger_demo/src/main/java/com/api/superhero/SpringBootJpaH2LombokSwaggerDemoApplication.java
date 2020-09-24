e697488209a2e1a33d0c04ae963f7866758f3784
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2020-09-24 17:14:45 +0530

    superheros API updated with swagger doc

--- phase3/spring_boot_jpa_h2_lombok_swagger_demo/src/main/java/com/api/superhero/SpringBootJpaH2LombokSwaggerDemoApplication.java
+++ phase3/spring_boot_jpa_h2_lombok_swagger_demo/src/main/java/com/api/superhero/SpringBootJpaH2LombokSwaggerDemoApplication.java
@@ -2,8 +2,13 @@ package com.api.superhero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
 public class SpringBootJpaH2LombokSwaggerDemoApplication {
 
 	public static void main(String[] args) {
